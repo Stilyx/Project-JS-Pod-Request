93ce0a45d06bbdf4e964cc1a540f5c4cf844b630
Daniel Nunes <dani.perfil.profissional@gmail.com>
2022-10-20 03:19:31 -0300

    first version of Pod-Request
    Other Mentor Dev Challenge

---
+++ script.js
@@ -0,0 +1,27 @@
+const button = document.querySelector('.email-form');
+const email = document.querySelector('#email');
+const p = document.createElement('p');
+button.appendChild(p);
+
+button.addEventListener('submit', e => {
+  e.preventDefault();
+  validEmail();
+});
+
+function validEmail() {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const test = regex.test(email.value);
+  let inputValue = button.childNodes[1].value;
+  const isValidEmail = inputValue === '' || !test;
+
+  if (isValidEmail) {
+    p.classList.add('error-message');
+    p.classList.remove('success-message');
+    p.innerText = 'Oops! Please check your email';
+  } else {
+    p.classList.add('success-message');
+    p.classList.remove('error-message');
+    p.innerText = 'Email Sent';
+    button.childNodes[1].value = '';
+  }
+}
